1797e6e755014c0a95456387e62a30b2f26bbbac
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-29 18:43:44 +0600

    tcp dail read

---
+++ creating_Server/tcp/dial-read/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Println(err)
+	}
+	defer conn.Close()
+
+	bs, err := ioutil.ReadAll(conn)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(bs))
+}
