18c2b91bdff6c8157da66715a76c350d75fd3f78
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-30 19:23:01 +0600

    unmarshal json

--- jsonEncodingDecoding/unmarshal/main.go
+++ jsonEncodingDecoding/unmarshal/main.go
@@ -6,13 +6,14 @@ import (
 )
 
 type Person struct {
-	Name string
-	Age  string
+	Name    string
+	Country string
+	Age     string
 }
 
 func main() {
-	birdJson := `{"Name": "pigeon","Age": "21 year"}`
+	birdJson := `{"Name": "Abdullah","Country":"Bangladesh","Age": "21 year"}`
 	var p Person
 	json.Unmarshal([]byte(birdJson), &p)
-	fmt.Printf("Name: %s, Age: %v\n", p.Name, p.Age)
+	fmt.Printf("Name: %s, Country: %s, Age: %s\n", p.Name, p.Country, p.Age)
 }
