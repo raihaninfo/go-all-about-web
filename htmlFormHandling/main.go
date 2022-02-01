1cabf7eb0202c5dc6ef610704d601645774d8276
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-01 18:59:54 +0600

    create server

---
+++ htmlFormHandling/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("template/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8082", nil)
+}
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
