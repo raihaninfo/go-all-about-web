f5221b89dea3b249ddfd99edfc3c979acc11547e
raihaninfo <raihanmahmudi35@gmail.com>
2022-02-01 19:20:39 +0600

    golang form handling

--- htmlFormHandling/main.go
+++ htmlFormHandling/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -13,8 +14,29 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/process", process)
 	http.ListenAndServe(":8082", nil)
 }
 func index(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	fname := r.FormValue("fname")
+	lname := r.FormValue("lname")
+	fmt.Println(fname, lname)
+
+	d := struct {
+		First string
+		Last  string
+	}{
+		First: fname,
+		Last:  lname,
+	}
+
+	tpl.ExecuteTemplate(w, "processor.gohtml", d)
 }
